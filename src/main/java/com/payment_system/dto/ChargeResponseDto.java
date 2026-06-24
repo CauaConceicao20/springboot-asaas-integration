5e146c1f31b8eb947fa182a8e5847934f8eadbd2
Cauã Conceição <cauaconceicaodev@hotmail.com>
2026-06-24 17:04:31 -0300

    fix: fixes response incompatibility with Asaas api

--- src/main/java/com/payment_system/dto/ChargeResponseDto.java
+++ src/main/java/com/payment_system/dto/ChargeResponseDto.java
@@ -1,86 +1,75 @@
 package com.payment_system.dto;
 
+import java.math.BigDecimal;
 import java.util.Set;
 
 public record ChargeResponseDto(
+        String id,
         String object,
         String hasMore,
         int totalCount,
         int limit,
+        String customer,
+        String dateCreated,
+        BigDecimal value,
+        BigDecimal netValue,
+        BigDecimal interestValue,
+        String billingType,
+        String pixTransaction,
+        String status,
+        String dueDate,
+        String originalDueDate,
+        String paymentDate,
+        String clientPaymentDate,
+        String installmentNumber,
+        String daysAfterDueDateToRegistrationCancellation,
+        String invoiceUrl,
+        String invoiceNumber,
+        String lastInvoiceViewedDate,
+        String lastBankSlipViewedDate,
+        Discount discount,
+        Fine fine,
+        Interest interest,
+        boolean postalService,
+        String digitalAccount,
+        String subscription,
+        String installment,
+        String creditCard,
+        boolean canBePaidAfterDueDate,
+        String pixQrCodeId,
+        String pixQrCode,
+        String pixExpirationDate,
+        String confirmedDate,
+        Escrow escrow,
+        Set<Refunds> refunds,
+        String externalReference,
+        boolean deleted,
+        boolean anticipated,
+        boolean anticipable,
+        String creditDate,
+        String estimatedCreditDate,
+        String transactionReceiptUrl,
+        String nossoNumero,
+        String bankSlipUrl,
+        BigDecimal originalValue,
+        String description,
+        String paymentLink,
+        String checkoutSession,
         int offset,
         Set<DataPaymentResponseDto> data
 ) {
 
     public record DataPaymentResponseDto(
-            String object,
-            String id,
-            String customer,
-            String subscription,
-            String installment,
-            String checkoutSession,
-            String paymentLink,
-            double value,
-            double netValue,
-            double originalValue,
-            double interestValue,
-            String description,
-            String billingType,
             CreditCard creditCard,
-            boolean canBePaidAfterDueDate,
-            String pixTransaction,
-            String pixQrCodeId,
-            String status,
-            String dueDate,
-            String originalDueDate,
-            String paymentDate,
-            String clientPaymentDate,
-            String installmentNumber,
-            String invoiceUrl,
-            String invoiceNumber,
-            String dateCreated,
-            String confirmedDate,
-            String anticipable,
-            String lastInvoiceViewedDate,
-            String lastBankSlipViewedDate,
-            String externalReference,
-            boolean deleted,
-            boolean anticipated,
-            String anticipatedDate,
-            String creditDate,
-            String estimatedCreditDate,
-            String transactionReceiptUrl,
-            String nossoNumero,
-            String bankSlipUrl,
-            Discount discount,
-            Fine fine,
-            Interest interest,
             Set<Split> split,
-            boolean postalService,
-            boolean daysAfterDueDateToRegistrationCancellation,
             Chargeback chargeback,
-            Escrow escrow,
-            Set<Refunds> refunds
+            String anticipatedDate
     ) {
         public record CreditCard(
                 String creditCardNumber,
                 String creditCardBrand,
                 String creditCardToken
 
-        ) {}
-
-        public record Discount(
-                String limitDate,
-                double value,
-                int dueDateLimitDays,
-                String type
-        ) {}
-        public record Fine(
-                double value
-        ) {}
-
-        public record Interest(
-                String type,
-                double value
         ) {}
 
         public record Split(
@@ -110,29 +99,47 @@ public record ChargeResponseDto(
                 String deadlineToSendDisputeDocuments
         ) {}
 
-        public record Escrow(
-                String id,
-                String status,
-                String expirationDate,
-                String finishDate,
-                String finishReason
-        ) {}
 
-        public record Refunds(
-                String dateCreated,
-                String status,
+    }
+    public record Discount(
+            String limitDate,
+            double value,
+            int dueDateLimitDays,
+            String type
+    ) {}
+
+    public record Fine(
+            String type,
+            double value
+    ) {}
+
+    public record Interest(
+            String type,
+            double value
+    ) {}
+
+    public record Escrow(
+            String id,
+            String status,
+            String expirationDate,
+            String finishDate,
+            String finishReason
+    ) {}
+
+    public record Refunds(
+            String dateCreated,
+            String status,
+            double value,
+            String endToEndIdentifier,
+            String description,
+            String effectiveDate,
+            String transactionReceiptUrl,
+            Set<RefundedSplits> refundedSplits
+    ) {
+        public record RefundedSplits(
+                String id,
                 double value,
-                String endToEndIdentifier,
-                String description,
-                String effectiveDate,
-                String transactionReceiptUrl,
-                Set<RefundedSplits> refundedSplits
-        ) {
-            public record RefundedSplits(
-                    String id,
-                    double value,
-                    boolean done
-            ) {}
-        }
+                boolean done
+        ) {}
     }
 }
